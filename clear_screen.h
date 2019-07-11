c2ae7d3d0bbf605d2001c723eee0e2b69ce16a43
MCRusher <Modernwarfare3Minecraft64@gmail.com>
2019-07-11 19:52:07 -0400

    Update clear_screen.h

--- clear_screen.h
+++ clear_screen.h
@@ -4,7 +4,7 @@
 #ifdef _WIN32
 static inline void clear_screen(void){ system("cls"); }
 #elif defined(unix) || defined(__unix) || defined(__unix__)
-static inline clear_screen(void){ system("clear"); }
+static inline void clear_screen(void){ system("clear"); }
 #else
 #error OS was not detected as modern windows or unix,\
 	   so an implementation of clear_screen is not available
