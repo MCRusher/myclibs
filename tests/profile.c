726f597830b2a1459f0e8318c6137d16340f4a55
MCRusher <Modernwarfare3Minecraft64@gmail.com>
2019-07-11 19:54:42 -0400

    Create profile.c
    Demonstrates a managed function

---
+++ tests/profile.c
@@ -0,0 +1,17 @@
+#include "newstd.h"
+
+int main(void)
+MANAGE
+  puts("What is your name?");
+  string name = string_ReadLn();
+  println_in("Hello, {}",name);
+  puts("How many years old are you?");
+  u8 age = string_ToU8(string_ReadLn());
+  puts("What is your sex?");
+  string sex = string_ReadLn();
+  puts("\nYour profile:");
+  println_in("- Name: \"{}\"",name);
+  printf("- Age: %d\n",age);
+  println_in("- Sex: {}",sex);
+  string_Delete(&name);//unnecessary but works fine.
+UNMANAGE
