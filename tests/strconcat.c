deafbda6484bb9c6cd29ae2908c0bae46d246dc4
MCRusher <Modernwarfare3Minecraft64@gmail.com>
2019-09-04 01:11:26 -0400

    Update strconcat.c

--- tests/strconcat.c
+++ tests/strconcat.c
@@ -1,6 +1,10 @@
 #include "newstd.h"
 
+//Returns an allocated string with a set prefix
 string PrefixedString(char const * const s){
+    //takes 2 string consts and returns a
+    //string containing both strings
+    //concatenated in the same order they were passed
     return string_Combine(
         //creates string without allocation,
         //result cannot safely be modified
@@ -12,5 +16,6 @@ string PrefixedString(char const * const s){
 int main(void){
     string s = PrefixedString("Hey");
     println(s);
+    //frees allocated string
     string_Delete(&s);
 }
