8ae0437421a5a2645889767cf3c7892e7c495405
MCRusher <Modernwarfare3Minecraft64@gmail.com>
2019-10-31 09:15:17 -0400

    Add files via upload
    Fixed bug in C11 branch
    
    integrated sleeping into threading.h since all sleep is per-thread anyways

--- threading.h
+++ threading.h
@@ -24,27 +24,19 @@ static inline thread_t thread_start(int(threaded *func)(),void* args){
 	}
 	return id;
 }
-///Creates a thread, starts it with or without a single arg, then returns an associated thread_t
+///Creates a thread, starts it with or without a single pointer arg, then returns an associated thread_t
 #define thread_start(func,args_ptr...) thread_start((func),(tool_NULL_FN(),##args_ptr))
 ///Returns a thread_t associated with the current thread
 static inline thread_t thread_this(void){ return (thread_t)GetCurrentThread(); }
 ///Hints the processor to allow other threads to run before the current thread
 static inline void thread_yield(void){ SwitchToThread(); }
-////Instantly terminates the thread, Not recommended.
-//static inline void thread_kill(thread_t id){
-//	if(TerminateThread((HANDLE)id,0)==0){
-//		fputs("Could not kill thread.\n",stderr);
-//      exit(-1);
-//	}
-//}
-
 static inline void thread_join(thread_t id, int* ret){
 	if(WaitForSingleObject((HANDLE)id,INFINITE)==WAIT_FAILED || !(!ret || GetExitCodeThread((HANDLE)id,(LPDWORD)ret)!=0)){
 		fputs("Could not join thread.\n",stderr);
         exit(-1);
 	}
 }
-///Waits for the specified thread to end, then stores the return int value in ret if specified
+///Waits for the specified thread to end, then stores the return int value at ret if specified
 #define thread_join(id,ret_ptr...) thread_join((id),(tool_NULL_FN(),##ret_ptr))
 ///Separates the execution of the specified thread from main thread
 static inline void thread_detach(thread_t id){
@@ -53,6 +45,7 @@ static inline void thread_detach(thread_t id){
         exit(-1);
 	}
 }
+static inline void thread_sleep(uint32_t ms){ Sleep(ms); }
 #elif defined(unix) || defined(__unix) || defined(__unix__)
 #include <pthread.h>
 #include <unistd.h>
@@ -68,25 +61,19 @@ static inline thread_t thread_start(int(threaded *func)(), void* args){
 	}
 	return id;
 }
-///Creates a thread, starts it with or without a single arg, then returns an associated thread_t
+///Creates a thread, starts it with or without a single pointer arg, then returns an associated thread_t
 #define thread_start(func,args_ptr...) thread_start((func),(tool_NULL_FN(),##args_ptr))
 ///Returns a thread_t associated with the current thread
 static inline thread_t thread_this(void){ return pthread_self(); }
 ///Hints the processor to allow other threads to run before the current thread
 static inline void thread_yield(void){ sched_yield(); }
-//static inline void thread_kill(thread_t id){
-//	if(pthread_cancel(id)!=0){
-//		fputs("Could not kill thread.\n",stderr);
-//      exit(-1);
-//	}
-//}
 static inline void thread_join(thread_t id, int* ret){
 	if(pthread_join(id,(void**)ret)!=0){
 		fputs("Could not join thread.\n",stderr);
         exit(-1);
 	}
 }
-///Waits for the specified thread to end, then stores the return int value in ret if specified
+///Waits for the specified thread to end, then stores the return int value at ret if specified
 #define thread_join(id,ret_ptr...) thread_join((id),(tool_NULL_FN(),##ret_ptr))
 ///Separates the execution of the specified thread from main thread
 static inline void thread_detach(thread_t id){
@@ -95,6 +82,7 @@ static inline void thread_detach(thread_t id){
         exit(-1);
     }
 }
+static inline void thread_sleep(uint32_t ms){ nanosleep((const struct timespec[]){{(ms/1000),(ms%1000)*1000000L}},((void*)0)); }
 #elif __STDC_VERSION__ >=  201112L && !defined( __STDC_NO_THREADS__)
 #include <threads.h>
 typedef thrd_t thread_t;
@@ -108,23 +96,19 @@ static inline thread_t thread_start(int(threaded *func)(), void* args){
 	}
 	return id;
 }
-///Creates a thread, starts it with or without a single arg, then returns an associated thread_t
+///Creates a thread, starts it with or without a single pointer arg, then returns an associated thread_t
 #define thread_start(func,args_ptr...) thread_start((func),(tool_NULL_FN(),##args_ptr))
 ///Returns a thread_t associated with the current thread
-static inline thread_t thread_this(void){ return thrd_current();
+static inline thread_t thread_this(void){ return thrd_current(); }
 ///Hints the processor to allow other threads to run before the current thread
 static inline void thread_yield(void){ thrd_yield(); }
-//static inline void thread_kill(thread_t id){
-//	fputs("Killing another thread is not available using current C11 <threads.h> features.\n",stderr);
-//	exit(-1);
-//}
 static inline void thread_join(thread_t id, int* ret){
-	if(thrd_join(id,(int*)ret)!=thrd_success){
+	if(thrd_join(id,ret)!=thrd_success){
 		fputs("Could not join thread.\n",stderr);
         exit(-1);
 	}
 }
-///Waits for the specified thread to end, then stores the return int value in ret if specified
+///Waits for the specified thread to end, then stores the return int value at ret if specified
 #define thread_join(id,ret_ptr...) thread_join((id),(tool_NULL_FN(),##ret_ptr))
 ///Separates the execution of the specified thread from main thread
 static inline void thread_detach(thread_t id){
@@ -133,7 +117,9 @@ static inline void thread_detach(thread_t id){
         exit(-1);
 	}
 }
+static inline void thread_sleep(uint32_t ms){ thrd_sleep((const struct timespec[]){{(ms/1000),(ms%1000)*1000000L}},((void*)0));}
 #else
 	#error OS not detected as modern windows or unix, and compiler does not support C11 threads, so no implementation of\
 		   threading.h is available
 #endif // _WIN32
+
